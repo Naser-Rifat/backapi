cafc90f0bda2b140e9b9b92b19fd344cc926751f
Naser Rifat <39612363+Naser-Rifat@users.noreply.github.com>
2022-02-22 00:56:34 +0600

    back api update

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
-
+require("dotenv").config();
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.40e4v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
