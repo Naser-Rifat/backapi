49833246e97dfcdc6d518fa105a5b63115d8003d
Naser Rifat <39612363+Naser-Rifat@users.noreply.github.com>
2022-02-22 09:29:19 +0600

    back api update

--- routes/Login.js
+++ routes/Login.js
@@ -2,20 +2,24 @@ const express = require("express");
 const router = express.Router();
 const db = require("../mongodb");
 
-router.get("/", async (req, res) => {
-  const email = req.query.email;
-  const password = req.query.password;
+router.post("/", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const query = { email: email };
+    console.log(email);
+    const result = await db.registrationdatacollection.findOne(query);
 
-  const query = { email: email };
-
-  const result = await db.registrationdatacollection.findOne(query);
-
-  if (result.email == email && result.password == password) {
-    console.log("Logged in Sucessfully with userid");
-    res.json({ data: "Logged in Sucessfully with userid" });
-  } else {
-    console.log("Error with code");
-    res.json({ data: "Error with code" });
+    if (result?.email == email && result?.password == password) {
+      console.log("Logged in Sucessfully with userid");
+      res.json({ data: "Logged in Sucessfully with userid" });
+    } else {
+      console.log("Error with code");
+      res.json({ data: "Error with code" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.json({ data: error.message });
   }
 });
 
--- routes/Registration.js
+++ routes/Registration.js
@@ -3,36 +3,42 @@ const router = express.Router();
 const db = require("../mongodb");
 
 router.post("/", async (req, res) => {
-  console.log(req.body);
-  const searchEmail = req.body.email;
-  const filter = { email: searchEmail };
-  const result = await db.registrationdatacollection.findOne(filter);
-  if (!result) {
-    const registrationInfo = req.body;
-    const name = registrationInfo.name;
-    const email = registrationInfo.email;
-    const password = registrationInfo.password;
-    const gender = registrationInfo.gender;
-    const age = registrationInfo.age;
-    const userInfo = {
-      name,
-      email,
-      password,
-      gender,
-      age,
-    };
+  try {
+    console.log(req.body);
+    const searchEmail = req.body.email;
+    const filter = { email: searchEmail };
+    const result = await db.registrationdatacollection.findOne(filter);
+    console.log(result);
+    if (!result) {
+      const registrationInfo = req.body;
+      const name = registrationInfo.name;
+      const email = registrationInfo.email;
+      const password = registrationInfo.password;
+      const gender = registrationInfo.gender;
+      const age = registrationInfo.age;
+      const userInfo = {
+        name,
+        email,
+        password,
+        gender,
+        age,
+      };
 
-    const result = await db.registrationdatacollection.insertOne(userInfo);
-    if (result.acknowledged == true) {
-      console.log("Inserted Successfully");
-      res.send({ data: "Inserted Successfully" });
+      const result = await db.registrationdatacollection.insertOne(userInfo);
+      if (result?.acknowledged == true) {
+        console.log("Inserted Successfully");
+        res.send({ data: "Inserted Successfully" });
+      } else {
+        console.log("error with this code");
+        res.send({ data: "error with this code" });
+      }
     } else {
-      console.log("error with this code");
-      res.send({ data: "error with this code" });
+      console.log("This email already exist");
+      res.send({ data: "This email already exist" });
     }
-  } else {
-    console.log("This email already exist");
-    res.send({ data: "This email already exist" });
+  } catch (error) {
+    console.log(error.message);
+    res.send({ data: error.message });
   }
 });
 
